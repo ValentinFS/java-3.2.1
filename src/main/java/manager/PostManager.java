fd08ad24973f2bd6984554e95c6cce5a3e3f0501
Valentin Fadeev <valentinfs@yandex.ru>
2021-09-13 23:29:39 +0300

    PostManager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+
+    public Post[] search(owner_id, domain, query, owners_only, count, offset) {
+
+        return null;
+    }
+
+    public void delete(owner_id, post_id) {
+    }
+
+}
